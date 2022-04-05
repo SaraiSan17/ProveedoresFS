120dff59b8f8d42756fef7b41a499d0322988c87
Omar Alvarado Conde <omar.alvarado@expertos-ti.com>
2022-04-05 01:34:46 -0500

    ADD permisos al componente de navegacion

--- front_end/src/app/services/auth.guard.ts
+++ front_end/src/app/services/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router, NavigationEnd, Event } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { EndpointsService } from './endpoints.service';
@@ -11,21 +11,45 @@ export class AuthGuard implements CanActivate {
 
   private urlPermisions = {
     ADMIN: ['*'],
-    PROVEEDOR: ['']
+    PROVEEDOR: ['/home', '/facturacion']
   }
 
-  constructor( private routes: Router, private Auth: AuthService){}
+  constructor(private routes: Router, private Auth: AuthService) { }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-      console.log("On guard",this.Auth.user)
-      if (this.Auth.isLoggedIn$) {
-        return true;
+    console.log("On guard", this.Auth.user)
+    let inRoute = false;
+    if (this.Auth.isLoggedIn$) {
+      if (this.Auth.user.Rol.rol == "ADMIN") {
+        inRoute = true;
       } else {
-        this.routes.navigate(['/']);
-        return false;
+        let from = this.routes.routerState.snapshot.url;
+        console.log("from", from)
+        this.routes.events.subscribe((event: Event) => {
+
+          if (event instanceof NavigationEnd) {
+            console.log(event.url)
+            console.log(event);
+
+
+            if (this.urlPermisions.PROVEEDOR.includes(event.url)) {
+              inRoute = true;
+            } else {
+              inRoute = false
+              this.routes.navigate([from]);
+            }
+          }
+        })
+
       }
+
+      return true;
+    } else {
+      this.routes.navigate(['/']);
+      return false;
+    }
   }
-  
+
 }
